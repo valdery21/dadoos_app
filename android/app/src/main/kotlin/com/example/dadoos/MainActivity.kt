108b00826ac048ef7c9887e541aef45cb71463bd
Valdery <valdery21@hotmail.com>
2023-04-26 15:12:00 -0300

    Dadoos

---
+++ android/app/src/main/kotlin/com/example/dadoos/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dadoos
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.teal.shade700,
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text(
+            'Dadoos',
+          ),
+          backgroundColor: Colors.teal.shade900,
+        ),
+        body: const Dadoos(),
+      ),
+    ),
+  );
+}
+
+class Dadoos extends StatefulWidget {
+  const Dadoos({Key key}) : super(key: key);
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _DadoosState createState() => _DadoosState();
+}
+
+class _DadoosState extends State<Dadoos> {
+  int numeroDadoEsquerda = 1;
+  int numeroDadoDireita = 1;
+
+  void alterarFacesDados() {
+    setState(() {
+      numeroDadoEsquerda = Random().nextInt(6) + 1;
+      numeroDadoDireita = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                alterarFacesDados();
+                // numeroDadoEsquerda = 4;
+              },
+              child: Image.asset('imagens/dado$numeroDadoEsquerda.png'),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                alterarFacesDados();
+
+                //print('Botão direito pressionado');
+              },
+              child: Image.asset('imagens/dado$numeroDadoDireita.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
